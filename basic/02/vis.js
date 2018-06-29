0dc2ad669a3c9da51a355813695f69674a65cc82
@micahstubbs <micah.stubbs@gmail.com>
2018-06-29 14:26:41 -0700

    add example with a new large dataset

---
+++ basic/02/vis.js
@@ -0,0 +1,37 @@
+fetch('blocks-citation-graph.json')
+  .then(res => res.json())
+  .then(data => {
+    const N = 300
+    const Graph = new ThreeForceGraph().graphData(data)
+
+    // Setup renderer
+    const renderer = new THREE.WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.getElementById('3d-graph').appendChild(renderer.domElement)
+
+    // Setup scene
+    const scene = new THREE.Scene()
+    scene.add(Graph)
+    scene.add(new THREE.AmbientLight(0xbbbbbb))
+
+    // Setup camera
+    const camera = new THREE.PerspectiveCamera()
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    camera.lookAt(Graph.position)
+    camera.position.z = Math.cbrt(N) * 100
+
+    // Add camera controls
+    const tbControls = new TrackballControls(camera, renderer.domElement)
+
+    // Kick-off renderer
+    ;(function animate() {
+      // IIFE
+      Graph.tickFrame()
+
+      // Frame cycle
+      tbControls.update()
+      renderer.render(scene, camera)
+      requestAnimationFrame(animate)
+    })()
+  })
